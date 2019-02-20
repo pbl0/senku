094f87612dde804c4aca6595a0668fbd0d464cd0
Antonio Miguel Pavon <Antonio Miguel Pavon@DESKTOP-9BOG7HL>
2019-02-20 09:13:00 +0100

    prueba de una vez

--- src/com/senku/view/Tablero.java
+++ src/com/senku/view/Tablero.java
@@ -5,8 +5,12 @@
  */
 package com.senku.view;
 
+import javafx.scene.Group;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Circle;
 
 
@@ -24,7 +28,21 @@ public class Tablero {
     
     
     public GridPane getGridTablero(){
-        ficha.setFill(Color.RED);
+        ficha.setCenterX(300); 
+        ficha.setCenterY(180); 
+//Asignamos los colores: Blanco para el centro de la bola, rojo para el anillo medio/exterior
+        Stop[] stops = new Stop[] { 
+         new Stop(0.0, Color.WHITE),  
+         new Stop(0.3, Color.RED)
+      };     
+// Aqui asignamos la funcion del "foco" que llega a la bola, para mas informacion cntrol+space
+      RadialGradient coloresRadial = 
+         new RadialGradient(0, 0, 300, 180, 60, false, CycleMethod.NO_CYCLE, stops);
+      
+      ficha.setFill(coloresRadial); 
+      
+      Group root = new Group(ficha);
+      
         gridTablero.setHgap(10);
         gridTablero.setVgap(10);
         gridTablero.add(ficha,3,0);
